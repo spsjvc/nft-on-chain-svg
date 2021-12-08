4e9c1a20ee7693857cc4e778717aa3e2fe5e0fc4
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2021-12-08 22:50:19 +0100

    Add the tokenURI method to contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
@@ -8,6 +9,31 @@ contract Token is ERC721, Ownable {
     uint private _tokenIndex = 0;
 
     constructor() ERC721("Token", "TKN") {
+    }
+
+    function _getSVG(uint256 tokenId) private pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "<svg viewBox=\\\"0 0 350 350\\\" xmlns=\\\"http://www.w3.org/2000/svg\\\" style=\\\"background-color: black;\\\">",
+                "<text x=\\\"5\\\" y=\\\"15\\\" fill=\\\"white\\\">Token #",
+                Strings.toString(tokenId),
+                "</text></svg>"
+            )
+        );
+    }
+
+    function tokenURI(uint256 tokenId) override public pure returns (string memory) {
+        string memory svg = _getSVG(tokenId);
+
+        return string(
+            abi.encodePacked(
+                "{\"name\":\"Token #",
+                Strings.toString(tokenId),
+                "\",\"image\":\"",
+                svg,
+                "\"}"
+            )
+        );
     }
 
     function mint() public {
